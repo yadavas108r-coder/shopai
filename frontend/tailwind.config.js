c4d30174fcd977aefe75aca0454f8e4cfee3590a
yadavas108r-coder <yadavas108r@gmail.com>
2025-10-21 14:59:22 +0530

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563EB',
+        accent: '#16A34A',
+        bg: '#F9FAFB'
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+};
